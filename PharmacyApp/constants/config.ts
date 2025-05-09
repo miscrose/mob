99fefa2c239dc286bae1d8c6108bfeaf9ebe8904
achraf <bastamyachraf@gmail.com>
2025-05-09 02:25:43 +0200

    test ci after pub

--- PharmacyApp/constants/config.ts
+++ PharmacyApp/constants/config.ts
@@ -1,2 +1,3 @@
 //export const API_URL = 'http://localhost:8080';
-export const API_URL = 'http://192.168.1.102:8080';
+//export const API_URL = 'http://192.168.1.102:8080';
+export const API_URL = 'https://mob-production.up.railway.app';
